abd274e8609c7fe6059c64882eb7835be186f047
Amalya <amalya.adyanissa@gmail.com>
2019-12-18 15:20:45 +0700

    batalin gitignore

---
+++ routers/pickProject.js
@@ -0,0 +1,20 @@
+const seniman = require('express').Router()
+const ControlSeniman = require('../Controller/ControlSeniman.js')
+
+//menampilkan list kategori seni untuk user yang login
+seniman.get('/:dataUser', ControlSeniman.tampilSeniman)
+// seniman.get('/', ControlSeniman.tampilSeniman)
+
+//menampilkan list seniman sesuai kategori dan ada slot
+seniman.get('/:dataUser/:name', ControlSeniman.tampilFilteredSeniman)
+// seniman.get('/:name', ControlSeniman.tampilFilteredSeniman)
+
+//menampilkan profil seniman
+seniman.get('/:dataUser/:tag/:id', ControlSeniman.tampilProfileSeniman)
+
+//nambah di isHired slot jgn ngurang
+//menghire seniman, nambahin di database conjunction
+seniman.post('/:dataUser/:tag/:id', ControlSeniman.hireSeniman)
+// seniman.post('/:tag/:id', ControlSeniman.hireSeniman)
+
+module.exports = seniman
